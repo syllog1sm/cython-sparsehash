34019f9d3de1a0f25e04af918a91f86a904097d3
Matthew Honnibal <matthew@honnibal.com>
2014-07-25 00:15:34 +0100

    * Fix module, so it can be imported properly.

--- setup.py
+++ setup.py
@@ -1,60 +1,41 @@
 #!/usr/bin/env python
-import Cython.Distutils
-from distutils.extension import Extension
-import distutils.core
+from distutils.core import setup
+from Cython.Distutils import Extension
+from Cython.Distutils import build_ext
+from glob import glob
 
 import sys
 import os
-from os.path import join as pjoin
+from os.path import splitext
+
+from distutils.sysconfig import get_python_inc
+INCLUDES = ['sparsehash/includes']
 
 
-def clean(ext):
-    for pyx in ext.sources:
-        if pyx.endswith('.pyx'):
-            c = pyx[:-4] + '.c'
-            cpp = pyx[:-4] + '.cpp'
-            so = pyx[:-4] + '.so'
-            if os.path.exists(so):
-                os.unlink(so)
-            if os.path.exists(c):
-                os.unlink(c)
-            elif os.path.exists(cpp):
-                os.unlink(cpp)
-
-
-pwd = os.path.dirname(__file__)
-
-includes = [os.path.join(pwd, 'sparsehash')]
-libs = [os.path.join(pwd, 'sparsehash')]
-
-exts = [
-    Extension('ext.murmurhash', ["sparsehash/murmurhash.pyx", "sparsehash/MurmurHash2.cpp",
-              "sparsehash/MurmurHash3.cpp"], language="c++", include_dirs=includes),
-    Extension('ext.sparsehash', ["sparsehash/sparsehash.pyx"], language="c++",
-              include_dirs=includes),
-]
-
-
-if sys.argv[1] == 'clean':
-    print >> sys.stderr, "cleaning .c, .c++ and .so files matching sources"
-    map(clean, exts)
-
-distutils.core.setup(
+setup(
+    name="sparsehash",
+    version="0.2",
+    author="Matthew Honnibal",
+    author_email="honnibal@gmail.com",
+    url="http://github.com/syllog1sm/cython-sparsehash",
+    packages=["sparsehash"],
+    package_data={"sparsehash": ["*.pxd"]},
+    description="""Cython declarations for the Google Sparsehash library.""",
+    setup_requires=['Cython >= 0.18'],
+    install_requires=['Cython >= 0.18'],
     classifiers=[
-        'Programming Language :: Python :: 2.7',
+                'Development Status :: 4 - Beta',
+                'Environment :: Console',
+                'Operating System :: OS Independent',
+                'Intended Audience :: Science/Research',
+                'License :: OSI Approved :: MIT',
+                'Programming Language :: Cython',
+                'Topic :: Scientific/Engineering'],
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = [
+        Extension('sparsehash.murmurhash',
+            sources=['sparsehash/murmurhash.pyx', 'src/MurmurHash2.cpp', 'src/MurmurHash3.cpp'],
+            include_dirs=INCLUDES
+        ),
     ],
-    description='Cython wrapper for sparsehash and MurmurHash',
-    keywords='sparsehash densehash murmur murmurhash cython',
-    license='MIT',
-    name='sparsehash',
-    packages=['sparsehash'],
-    author='Matthew Honnibal',
-    author_email='honnibal@gmail.com',
-    version='0.11',
-    url='http://github.com/syllog1sm/cython-sparsehash',
-    cmdclass={'build_ext': Cython.Distutils.build_ext},
-    ext_modules=exts,
 )
-
-
-
